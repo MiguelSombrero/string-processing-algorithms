fb0eab6af7022810129ffd3b5a41a6edcc4a48e0
Miika Somero <miika.somero@alfame.com>
2022-11-01 08:32:51 +0200

    added tests for recursive fibonacci calculator

---
+++ excercise1/FibonacciCalculator/src/test/java/com/github/miguelsombrero/fibonacci/FibonacciCalculatorTest.java
@@ -0,0 +1,45 @@
+package com.github.miguelsombrero.fibonacci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class FibonacciCalculatorTest {
+    private int input;
+    private int expected;
+
+    public FibonacciCalculatorTest(int input, int expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection fibonacciNumbers() {
+        return Arrays.asList(new Object[][] {
+                { 3, 2 },
+                { 4, 3 },
+                { 5, 5 },
+                { 6, 8 },
+                { 7, 13 },
+                { 8, 21 },
+                { 9, 34 },
+                { 10, 55 },
+                { 11, 89 },
+                { 12, 144 }
+        });
+    }
+
+    @Test
+    public void recursiveFibonacciCalculatorWorksCorrectly() {
+        FibonacciCalculator calculator = new RecursiveFibonacciCalculator();
+        assertEquals(expected, calculator.calculate(input));
+    }
+
+
+}
